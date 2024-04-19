aa32b8ee4ceb1a399beb2100a1294e24f774d93f
Danilka06 <main@0x1f44.ru>
2024-04-19 10:14:20 +0300

    add: page traveling with sidebar

--- app/views.py
+++ app/views.py
@@ -6,3 +6,19 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
